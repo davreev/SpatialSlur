9768b53e54ad94f85eceb22132ad3cb7146b580a
David Reeves <darthur.reeves@gmail.com>
2017-11-19 16:00:42 +0000

    Separate handle types

---
+++ SpatialSlur/SlurDynamics/BodyHandle.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SpatialSlur.SlurCore;
+
+/*
+ * Notes
+ */
+
+namespace SpatialSlur.SlurDynamics
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public class BodyHandle : ParticleHandle, IHandle
+    {
+        /// <summary></summary>
+        public Vec3d AngleDelta;
+        /// <summary></summary>
+        public double AngleWeight;
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public BodyHandle()
+        {
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="index"></param>
+        public BodyHandle(int index)
+            :base(index)
+        {
+        }
+
+
+        #region Explicit interface implementations
+
+        /// <summary></summary>
+        Vec3d IHandle.AngleDelta
+        {
+            get { return AngleDelta; }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        double IHandle.AngleWeight
+        {
+            get { return AngleWeight; }
+        }
+
+        #endregion
+    }
+}
---
+++ SpatialSlur/SlurDynamics/ParticleHandle.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SpatialSlur.SlurCore;
+
+/*
+ * Notes
+ */
+
+namespace SpatialSlur.SlurDynamics
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public class ParticleHandle : IHandle
+    {
+        #region Static
+
+        /// <summary>
+        /// Implicitly converts a handle to its index for convenience.
+        /// </summary>
+        /// <param name="handle"></param>
+        public static implicit operator int(ParticleHandle handle)
+        {
+            return handle.Index;
+        }
+
+        #endregion
+
+
+        /// <summary></summary>
+        public Vec3d Delta;
+        /// <summary></summary>
+        public double Weight;
+        /// <summary></summary>
+        public int Index = -1;
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ParticleHandle()
+        {
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="index"></param>
+        public ParticleHandle(int index)
+        {
+            Index = index;
+        }
+
+
+        #region Explicit interface implementations
+
+        /// <summary></summary>
+        Vec3d IHandle.Delta
+        {
+            get { return Delta; }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        double IHandle.Weight
+        {
+            get { return Weight; }
+        }
+
+
+        /// <summary></summary>
+        Vec3d IHandle.AngleDelta
+        {
+            get { return Vec3d.Zero; }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        double IHandle.AngleWeight
+        {
+            get { return 0.0; }
+        }
+
+
+        /// <summary></summary>
+        int IHandle.Index
+        {
+            get { return Index; }
+            set { Index = value; }
+        }
+
+        #endregion
+    }
+}
