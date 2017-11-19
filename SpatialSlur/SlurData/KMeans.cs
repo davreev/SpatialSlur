560f964e65095e31808b8f81a4f0e74ccc0b2064
David Reeves <darthur.reeves@gmail.com>
2017-11-19 16:17:16 +0000

    Add support for parallel execution

--- SpatialSlur/SlurData/KMeans.cs
+++ SpatialSlur/SlurData/KMeans.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 using SpatialSlur.SlurCore;
@@ -27,7 +29,7 @@ namespace SpatialSlur.SlurData
 
 
         /// <summary>
-        /// 
+        /// K cluster centers are randomly selected from the given set of data points.
         /// </summary>
         /// <param name="points"></param>
         /// <param name="k"></param>
@@ -39,7 +41,7 @@ namespace SpatialSlur.SlurData
 
 
         /// <summary>
-        /// 
+        /// K cluster centers are randomly selected from the given set of data points.
         /// </summary>
         /// <param name="points"></param>
         /// <param name="k"></param>
@@ -48,7 +50,37 @@ namespace SpatialSlur.SlurData
         {
             _points = points;
             _nearestClusters = new int[points.Length];
+
             InitClusters(k, seed);
+        }
+
+
+        /// <summary>
+        /// Initial cluster centers are explicitly defined.
+        /// </summary>
+        /// <param name="points"></param>
+        /// <param name="k"></param>
+        /// <param name="seed"></param>
+        public KMeans(IEnumerable<double[]> points, IEnumerable<double[]> clusterCenters)
+            : this(points.ToArray(), clusterCenters.ToArray())
+        {
+        }
+
+
+        /// <summary>
+        /// Initial cluster centers are explicitly defined.
+        /// </summary>
+        /// <param name="points"></param>
+        /// <param name="k"></param>
+        /// <param name="seed"></param>
+        public KMeans(double[][] points, double[][] clusterCenters)
+        {
+            _points = points;
+            _nearestClusters = new int[points.Length];
+
+            _clusterCenters = clusterCenters;
+            _clusterSizes = new int[clusterCenters.Length];
+            _clusterIndices = Enumerable.Range(0, clusterCenters.Length).ToArray();
         }
 
 
@@ -136,14 +168,25 @@ namespace SpatialSlur.SlurData
         /// </summary>
         /// <param name="maxSteps"></param>
         /// <returns></returns>
-        public int Cluster(int maxSteps)
+        public int Cluster(int maxSteps, bool parallel = false)
         {
             int count = 0;
 
-            while (count++ < maxSteps)
+            if(parallel)
             {
-                if (UpdateNearest() == 0) return count;
-                UpdateClusterCenters();
+                while (count++ < maxSteps)
+                {
+                    if (UpdateNearestParallel() == 0) return count;
+                    UpdateClusterCenters();
+                }
+            }
+            else
+            {
+                while (count++ < maxSteps)
+                {
+                    if (UpdateNearest() == 0) return count;
+                    UpdateClusterCenters();
+                }
             }
 
             return -1;
@@ -158,7 +201,7 @@ namespace SpatialSlur.SlurData
             // create balanced tree of cluster centers
             var tree = KdTree<int>.CreateBalanced(_clusterCenters, _clusterIndices);
 
-            // for each data point, find nearest cluster center
+            // for each data point, find the nearest cluster center
             int count = 0;
             for (int i = 0; i < _points.Length; i++)
             {
@@ -171,7 +214,36 @@ namespace SpatialSlur.SlurData
                     count++;
                 }
             }
+            
+            return count;
+        }
+        
 
+        /// <summary>
+        /// Returns the number of data points that changed clusters.
+        /// </summary>
+        private int UpdateNearestParallel()
+        {
+            // create balanced tree of cluster centers
+            var tree = KdTree<int>.CreateBalanced(_clusterCenters, _clusterIndices);
+
+            // for each data point, find the nearest cluster center
+            int count = 0;
+            Parallel.ForEach(Partitioner.Create(0, _points.Length), range =>
+             {
+                 for (int i = range.Item1; i < range.Item2; i++)
+                 {
+                     var nearest = tree.NearestL2(_points[i]);
+
+                     // update if new nearest is different than previous
+                     if (nearest != _nearestClusters[i])
+                     {
+                         _nearestClusters[i] = nearest;
+                         Interlocked.Increment(ref count);
+                     }
+                 }
+             });
+            
             return count;
         }
 
@@ -179,7 +251,7 @@ namespace SpatialSlur.SlurData
         /// <summary>
         ///
         /// </summary>
-        void UpdateClusterCenters()
+        private void UpdateClusterCenters()
         {
             _clusterSizes.Clear();
 
