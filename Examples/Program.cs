f73e6f053ad2f68be43659f5f1646c32155a35b5
David Reeves <darthur.reeves@gmail.com>
2017-11-24 01:24:24 +0000

    Update examples

---
+++ Examples/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using SpatialSlur.SlurCore;
+using SpatialSlur.SlurMesh;
+
+/*
+ * Notes
+ */
+
+namespace SpatialSlur.Examples
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    static class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static void Main()
+        {
+            PlanarizeHeMesh.Start();
+        }
+    }
+}
