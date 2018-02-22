e5b4d11766491b60de441e6f1b94d8872653b6f6
darthur.reeves <darthur.reeves@gmail.com>
2018-02-22 20:39:51 +0000

    Cleanup extension methods

---
+++ SpatialSlur/SlurField/Extensions/GridField3dFactoryExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+using SpatialSlur.SlurCore;
+
+/*
+ * Notes 
+ */
+
+namespace SpatialSlur.SlurField
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class GridField3dFactoryExtensions
+    {
+        #region Vec3d
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static GridField3d<Vec3d> CreateFromFGA(this GridField3dFactory<Vec3d> factory, string path)
+        {
+            var content = File.ReadAllText(path, Encoding.ASCII);
+            var values = content.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int nx = int.Parse(values[0]);
+            int ny = int.Parse(values[1]);
+            int nz = int.Parse(values[2]);
+
+            Vec3d p0 = new Vec3d(
+                double.Parse(values[3]),
+                double.Parse(values[4]),
+                double.Parse(values[5]));
+
+            Vec3d p1 = new Vec3d(
+               double.Parse(values[6]),
+               double.Parse(values[7]),
+               double.Parse(values[8]));
+
+            var result = factory.Create(nx, ny, nz, new Interval3d(p0, p1));
+            var vecs = result.Values;
+            int index = 0;
+
+            for (int i = 9; i < values.Length; i += 3)
+            {
+                vecs[index++] = new Vec3d(
+                    double.Parse(values[i]),
+                    double.Parse(values[i + 1]),
+                    double.Parse(values[i + 2]));
+            }
+
+            return result;
+        }
+
+        #endregion
+    }
+}
---
+++ SpatialSlur/SlurField/Extensions/ListExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SpatialSlur.SlurCore;
+
+/*
+ * Notes
+ */ 
+
+namespace SpatialSlur.SlurField.Extensions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class ListExtensions
+    {
+        #region List<IDWPoint3d<T>>
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="points"></param>
+        /// <param name="point"></param>
+        /// <param name="value"></param>
+        /// <param name="influence"></param>
+        public static void Add<T>(this List<IDWPoint3d<T>> points, Vec3d point, T value, double influence = 1.0)
+        {
+            points.Add(IDWPoint3d.Create(point, value, influence));
+        }
+
+        #endregion
+    }
+}
