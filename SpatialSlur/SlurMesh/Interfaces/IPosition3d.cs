843aa8ad6a074274eff247ba6e98660acd1f702e
David Reeves <darthur.reeves@gmail.com>
2018-02-25 01:55:50 +0000

    Add vertex property interfaces

---
+++ SpatialSlur/SlurMesh/Interfaces/IPosition3d.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using SpatialSlur.SlurCore;
+
+/*
+ * Notes 
+ */
+
+namespace SpatialSlur.SlurMesh
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IPosition3d
+    {
+        /// <summary></summary>
+        Vec3d Position { get; set; }
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public static class IPosition3d<T>
+        where T : IPosition3d
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly Func<T, Vec3d> Get = v => v.Position;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static readonly Action<T, Vec3d> Set = (v, p) => v.Position = p; 
+    }
+}
--- SpatialSlur/SlurMesh/Interfaces/IVertex3d.cs
+++ SpatialSlur/SlurMesh/Interfaces/IVertex3d.cs
@@ -1,47 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using SpatialSlur.SlurCore;
-
-/*
+﻿/*
  * Notes
- */ 
+ */
 
 namespace SpatialSlur.SlurMesh
 {
     /// <summary>
     /// 
     /// </summary>
-    public interface IVertex3d
-    {
-        /// <summary></summary>
-        Vec3d Position { get; set; }
-
-        /// <summary></summary>
-        Vec3d Normal { get; set; }
-
-        /// <summary></summary>
-        Vec2d Texture { get; set; }
-    }
-
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <typeparam name="V"></typeparam>
-    public static class IVertex3dStatic<V>
-        where V : IVertex3d
-    {
-        public static readonly Func<V, Vec3d> GetPosition = v => v.Position;
-        public static readonly Action<V, Vec3d> SetPosition = (v, p) => v.Position = p;
-
-        public static readonly Func<V, Vec3d> GetNormal = v => v.Normal;
-        public static readonly Action<V, Vec3d> SetNormal = (v, n) => v.Normal = n;
-
-        public static readonly Func<V, Vec2d> GetTexture = v => v.Texture;
-        public static readonly Action<V, Vec2d> SetTexture = (v, t) => v.Texture = t;
-    }
+    public interface IVertex3d: IPosition3d, INormal3d {}
 }
