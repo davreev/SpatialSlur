591c72bdd1fe51ab2230ee734d14ed6a696407e7
David Reeves <darthur.reeves@gmail.com>
2018-03-02 02:24:54 +0000

    Add IDWMesh3d

---
+++ SpatialSlur/SlurRhino/Extensions/ListExtensions.cs
@@ -0,0 +1,38 @@
+﻿
+/*
+ * Notes
+ */
+
+#if USING_RHINO
+
+using Rhino.Geometry;
+using SpatialSlur.SlurField;
+using System.Collections.Generic;
+
+namespace SpatialSlur.SlurRhino
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class ListExtensions
+    {
+        #region List<IDWMesh3d<T>>
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="objects"></param>
+        /// <param name="point"></param>
+        /// <param name="value"></param>
+        /// <param name="influence"></param>
+        public static void Add<T>(this List<IDWObject3d<T>> objects, Mesh mesh, T value, double influence = 1.0)
+        {
+            objects.Add(IDWMesh3d.Create(mesh, value, influence));
+        }
+
+        #endregion
+    }
+}
+
+#endif
