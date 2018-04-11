df9d90190b75b41094f042b822e478bbf8340f21
David Reeves <darthur.reeves@gmail.com>
2018-04-11 12:16:49 +0100

    Add SDF primitives

--- SpatialSlur/SlurField/SDFUtil.cs
+++ SpatialSlur/SlurField/SDFUtil.cs
@@ -34,10 +34,27 @@ namespace SpatialSlur.SlurField
         /// <param name="point"></param>
         /// <param name="scale"></param>
         /// <returns></returns>
-        public static double Box(Vec3d point, Vec3d size)
+        public static double Box(Vec3d point, Vec3d scale)
         {
-            // TODO
-            throw new NotImplementedException();
+            var d = Vec3d.Abs(point) - scale;
+            return Math.Min(d.ComponentMax, 0.0) + Vec3d.Min(d, 0.0).Length;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="point"></param>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <param name="radius"></param>
+        /// <returns></returns>
+        public static double Capsule(Vec3d point, Vec3d start, Vec3d end, double radius)
+        {
+            var d0 = point - start;
+            var d1 = end - start;
+            var t = SlurMath.Saturate(Vec3d.Dot(d0, d1) / d1.SquareLength);
+            return (d0 - d1 * t).Length - radius;
         }
 
 
