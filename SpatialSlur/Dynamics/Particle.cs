12a3ceae9c1b11c365d23974051b80aace76fe51
David Reeves <darthur.reeves@gmail.com>
2018-12-07 13:07:32 +0000

    Refactor Particle

---
+++ SpatialSlur/Dynamics/Particle.cs
@@ -0,0 +1,94 @@
+﻿
+/*
+ * Notes
+ */
+
+using System;
+
+namespace SpatialSlur.Dynamics
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public readonly struct Particle
+    {
+        /// <summary></summary>
+        public readonly ParticlePosition Position;
+
+        /// <summary></summary>
+        public readonly ParticleRotation Rotation;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="position"></param>
+        public Particle(Vector3d position)
+        {
+            Position = new ParticlePosition(position);
+            Rotation = null;
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="rotation"></param>
+        public Particle(Quaterniond rotation)
+        {
+            Position = null;
+            Rotation = new ParticleRotation(rotation);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="position"></param>
+        /// <param name="rotation"></param>
+        public Particle(Vector3d position, Quaterniond rotation)
+        {
+            Position = new ParticlePosition(position);
+            Rotation = new ParticleRotation(rotation);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="position"></param>
+        /// <param name="rotation"></param>
+        public Particle(ParticlePosition position, ParticleRotation rotation)
+        {
+            Position = position;
+            Rotation = rotation;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="other"></param>
+        private Particle(Particle other)
+        {
+            Position = other.Position?.Duplicate();
+            Rotation = other.Rotation?.Duplicate();
+        }
+
+        /// <summary>
+        /// Returns a deep copy of this particle
+        /// </summary>
+        /// <returns></returns>
+        public Particle Duplicate()
+        {
+            return new Particle(this);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="position"></param>
+        /// <param name="rotation"></param>
+        public void Deconstruct(out ParticlePosition position, out ParticleRotation rotation)
+        {
+            position = Position;
+            rotation = Rotation;
+        }
+    }
+}
---
+++ SpatialSlur/Dynamics/ParticleRotation.cs
@@ -0,0 +1,136 @@
+﻿
+/*
+ * Notes
+ */
+
+using System;
+
+namespace SpatialSlur.Dynamics
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ParticleRotation
+    {
+        /// <summary>Current rotation</summary>
+        public Quaterniond Current = Quaterniond.Identity;
+
+        /// <summary>Angular velocity</summary>
+        public Vector3d Velocity;
+
+        /// <summary></summary>
+        internal Vector3d TorqueSum;
+
+        /// <summary></summary>
+        internal Vector3d DeltaSum;
+
+        /// <summary></summary>
+        internal double WeightSum;
+        
+        /// <summary></summary>
+        private Vector3d _inertiaInv = new Vector3d(1.0);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ParticleRotation()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="current"></param>
+        public ParticleRotation(Quaterniond current)
+        {
+            Current = current;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="other"></param>
+        private ParticleRotation(ParticleRotation other)
+        {
+            Current = other.Current;
+            Velocity = other.Velocity;
+            _inertiaInv = other._inertiaInv;
+        }
+
+        /// <summary>
+        /// Returns a deep copy of this object.
+        /// </summary>
+        /// <returns></returns>
+        public ParticleRotation Duplicate()
+        {
+            return new ParticleRotation(this);
+        }
+
+        /// <summary>
+        /// Gets/sets the rotational mass of the body.
+        /// Assumes the current rotation is aligned with the body's principal axes of inertia.
+        /// </summary>
+        public Vector3d Inertia
+        {
+            set
+            {
+                if (value.ComponentMin > 0.0)
+                    _inertiaInv = 1.0 / value;
+                else
+                    throw new ArgumentException("The value must be greater than zero.");
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Vector3d InertiaInv
+        {
+            get => _inertiaInv;
+            set
+            {
+                if (value.ComponentMin < 0.0)
+                    throw new ArgumentException("The value cannot be negative");
+
+                _inertiaInv = value;
+            }
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="torque"></param>
+        public void AddTorque(Vector3d torque)
+        {
+            TorqueSum += torque;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="delta"></param>
+        /// <param name="weight"></param>
+        public void AddDelta(Vector3d delta, double weight)
+        {
+            DeltaSum += delta * weight;
+            WeightSum += weight;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void ClearTorque()
+        {
+            TorqueSum = Vector3d.Zero;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void ClearDeltas()
+        {
+            DeltaSum = Vector3d.Zero;
+            WeightSum = 0.0;
+        }
+    }
+}
