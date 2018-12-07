21ccb158307f2e23f0e220e813aa94de36093004
David Reeves <darthur.reeves@gmail.com>
2018-12-07 13:06:31 +0000

    Add group structures

---
+++ SpatialSlur/Dynamics/ForceGroup.cs
@@ -0,0 +1,83 @@
+﻿
+/*
+ * Notes
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using SpatialSlur.Collections;
+
+namespace SpatialSlur.Dynamics
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ForceGroup : Impl.InfluenceGroup, IEnumerable<IForce>
+    {
+        private List<IForce> _forces;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ForceGroup()
+        {
+            _forces = new List<IForce>();
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="capacity"></param>
+        public ForceGroup(int capacity)
+        {
+            _forces = new List<IForce>(capacity);
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<IForce> Forces
+        {
+            get => _forces;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="particles"></param>
+        public void Apply(ReadOnlyArrayView<Particle> particles)
+        {
+            Apply(_forces, particles);
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<IForce>.Enumerator GetEnumerator()
+        {
+            return _forces.GetEnumerator();
+        }
+
+
+        #region Explicit interface implementations
+
+        IEnumerator<IForce> IEnumerable<IForce>.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
+    }
+}
---
+++ SpatialSlur/Dynamics/Impl/InfluenceGroup.cs
@@ -0,0 +1,90 @@
+﻿
+/*
+ * Notes
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+using SpatialSlur.Collections;
+
+using static System.Threading.Tasks.Parallel;
+
+namespace SpatialSlur.Dynamics.Impl
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class InfluenceGroup
+    {
+        private bool _parallel;
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool Parallel
+        {
+            get => _parallel;
+            set => _parallel = value;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="influences"></param>
+        /// <param name="particles"></param>
+        protected void Apply<T>(List<T> influences, ReadOnlyArrayView<Particle> particles)
+            where T : IInfluence
+        {
+            if (_parallel)
+                ApplyParallel(influences, particles);
+            else
+                ApplySerial(influences, particles);
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="influences"></param>
+        /// <param name="particles"></param>
+        private static void ApplySerial<T>(List<T> influences, ReadOnlyArrayView<Particle> particles)
+            where T: IInfluence
+        {
+            foreach (var infl in influences)
+            {
+                infl.Calculate(particles);
+                infl.Apply(particles);
+            }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="influences"></param>
+        /// <param name="particles"></param>
+        private static void ApplyParallel<T>(List<T> influences, ReadOnlyArrayView<Particle> particles)
+            where T : IInfluence
+        {
+            ForEach(Partitioner.Create(0, influences.Count), range => Body(range.Item1, range.Item2));
+
+            void Body(int from, int to)
+            {
+                for (int i = from; i < to; i++)
+                    influences[i].Calculate(particles);
+            }
+            
+            // Must apply influences serially to avoid race conditions
+            foreach (var infl in influences)
+                infl.Apply(particles);
+        }
+    }
+}
