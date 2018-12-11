5da690ab4d9d284fe9426d979083f6fca1ea0a12
David Reeves <darthur.reeves@gmail.com>
2018-12-11 09:17:21 +0000

    Add forces

---
+++ SpatialSlur/Dynamics/Forces/ForceField.cs
@@ -0,0 +1,108 @@
+﻿
+/*
+ * Notes
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using SpatialSlur.Collections;
+using SpatialSlur.Fields;
+
+using static System.Threading.Tasks.Parallel;
+
+namespace SpatialSlur.Dynamics.Forces
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Serializable]
+    public class ForceField : PositionForceBase
+    {
+        private IField3d<Vector3d> _field;
+        private double _strength;
+        private bool _parallel;
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="field"></param>
+        /// <param name="strength"></param>
+        public ForceField(IField3d<Vector3d> field, double strength = 1.0)
+        {
+            Field = field;
+            _strength = strength;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="field"></param>
+        /// <param name="strength"></param>
+        public ForceField(IEnumerable<int> indices, IField3d<Vector3d> field, double strength = 1.0)
+            : base(indices)
+        {
+            Field = field;
+            _strength = strength;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public IField3d<Vector3d> Field
+        {
+            get => _field;
+            set => _field = value ?? throw new ArgumentNullException();
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public double Strength
+        {
+            get => _strength;
+            set => _strength = value;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool Parallel
+        {
+            get => _parallel;
+            set => _parallel = value;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="particles"></param>
+        /// <param name="indices"></param>
+        /// <param name="deltas"></param>
+        protected override void Calculate(ReadOnlyArrayView<Particle> particles, ReadOnlyArrayView<int> indices, ArrayView<Vector3d> deltas)
+        {
+            if (_parallel)
+                ForEach(Partitioner.Create(0, indices.Count), range => Calculate(range.Item1, range.Item2));
+            else
+                Calculate(0, indices.Count);
+
+            void Calculate(int from, int to)
+            {
+                var field = _field;
+                var strength = _strength;
+
+                for (int i = from; i < to; i++)
+                    deltas[i] = field.ValueAt(particles[indices[i]].Position.Current) * strength;
+            }
+        }
+    }
+}
---
+++ SpatialSlur/Dynamics/Forces/LinearLoad.cs
@@ -0,0 +1,95 @@
+﻿
+/*
+ * Notes
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using SpatialSlur.Collections;
+
+namespace SpatialSlur.Dynamics.Forces
+{
+    /// <summary>
+    /// Applies a force proportional to the distance between 2 particles.
+    /// </summary>
+    [Serializable]
+    public class LinearLoad : IForce
+    {
+        private Vector3d _d0;
+        private int _i0, _i1;
+
+        private Vector3d _force;
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="index0"></param>
+        /// <param name="index1"></param>
+        /// <param name="force"></param>
+        /// <param name="massPerLength"></param>
+        /// <param name="strength"></param>
+        public LinearLoad(int index0, int index1, Vector3d force)
+        {
+            _i0 = index0;
+            _i1 = index1;
+            _force = force;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Index0
+        {
+            get { return _i0; }
+            set { _i0 = value; }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Index1
+        {
+            get { return _i1; }
+            set { _i1 = value; }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Vector3d Force
+        {
+            get { return _force; }
+            set { _force = value; }
+        }
+
+
+        /// <inheritdoc />
+        /// 
+        public void Calculate(ReadOnlyArrayView<Particle> bodies)
+        {
+            var d = bodies[_i1].Position.Current - bodies[_i0].Position.Current;
+            _d0 = _force * (d.Length * 0.5);
+        }
+
+
+        /// <inheritdoc />
+        /// 
+        public void Apply(ReadOnlyArrayView<Particle> bodies)
+        {
+            bodies[_i0].Position.ForceSum += _d0;
+            bodies[_i1].Position.ForceSum += _d0;
+        }
+
+
+        #region Explicit interface implementations
+        
+        void IInfluence.Initialize(ReadOnlyArrayView<Particle> particles) { }
+
+        #endregion
+    }
+}
