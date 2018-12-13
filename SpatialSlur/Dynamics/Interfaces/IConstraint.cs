d35f5c7a1a08093bc253a19054a48fe203f3a474
David Reeves <darthur.reeves@gmail.com>
2018-12-13 11:13:53 +0000

    Add ValueTuple element names

--- SpatialSlur/Dynamics/Interfaces/IConstraint.cs
+++ SpatialSlur/Dynamics/Interfaces/IConstraint.cs
@@ -22,6 +22,8 @@ namespace SpatialSlur.Dynamics
         /// Accumulates the calculated deltas onto the affected particles.
         /// </summary>
         /// <param name="particles"></param>
-        void Accumulate(ArrayView<(Vector3d, double)> linearDeltaSum, ArrayView<(Vector3d, double)> angularDeltaSum);
+        void Accumulate(
+            ArrayView<(Vector3d Delta, double Weight)> linearSum, 
+            ArrayView<(Vector3d Delta, double Weight)> angularSum);
     }
 }
