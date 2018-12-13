8b5cc34849e7afe66c8a9bfbb3560874728aadab
David Reeves <darthur.reeves@gmail.com>
2018-12-13 08:54:40 +0000

    Add abstract base types

---
+++ SpatialSlur/Dynamics/Impl/WeightedInfluence.cs
@@ -0,0 +1,35 @@
+﻿
+/*
+ * Notes
+ */
+
+using System;
+
+namespace SpatialSlur.Dynamics.Impl
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TDelta"></typeparam>
+    public abstract class WeightedInfluence<TDelta> : InfluenceBase<TDelta>
+    {
+        private double _weight;
+
+
+        /// <inheritdoc/>
+        /// <summary>
+        /// 
+        /// </summary>
+        public double Weight
+        {
+            get { return _weight; }
+            set
+            {
+                if (value < 0.0)
+                    throw new ArgumentOutOfRangeException("The value cannot be negative.");
+
+                _weight = value;
+            }
+        }
+    }
+}
