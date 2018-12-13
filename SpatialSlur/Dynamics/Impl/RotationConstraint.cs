0c868f170ecde85af70852693d6b820bbb2ee18f
David Reeves <darthur.reeves@gmail.com>
2018-12-13 11:11:49 +0000

    Add ValueTuple element names

--- SpatialSlur/Dynamics/Impl/RotationConstraint.cs
+++ SpatialSlur/Dynamics/Impl/RotationConstraint.cs
@@ -10,10 +10,12 @@ namespace SpatialSlur.Dynamics.Impl
     /// <summary>
     /// Base class for a constraint that acts on the rotations of a dynamic collection of particles.
     /// </summary>
-    public abstract class RotationConstraint : WeightedInfluence<(Vector3d, Vector3d)>, IConstraint
+    public abstract class RotationConstraint : WeightedInfluence<Vector3d>, IConstraint
     {
         /// <inheritdoc />
-        public virtual void Accumulate(ArrayView<(Vector3d, double)> linearDeltaSum, ArrayView<(Vector3d, double)> angularDeltaSum)
+        public virtual void Accumulate(
+            ArrayView<(Vector3d Delta, double Weight)> linearSum, 
+            ArrayView<(Vector3d Delta, double Weight)> angularSum)
         {
             var handles = Handles;
             var deltas = Deltas;
@@ -21,9 +23,9 @@ namespace SpatialSlur.Dynamics.Impl
 
             for (int i = 0; i < handles.Count; i++)
             {
-                ref var dr = ref angularDeltaSum[handles[i].RotationIndex];
-                dr.Item1 += deltas[i].Item2 * weight;
-                dr.Item2 += weight;
+                ref var dr = ref angularSum[handles[i].RotationIndex];
+                dr.Delta += deltas[i] * weight;
+                dr.Weight += weight;
             }
         }
     }
