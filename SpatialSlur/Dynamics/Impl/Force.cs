be749bb250f6325ff5e9cf0a1c7087224d698a2c
David Reeves <darthur.reeves@gmail.com>
2019-01-03 04:43:37 +0000

    Remove constructors

--- SpatialSlur/Dynamics/Impl/Force.cs
+++ SpatialSlur/Dynamics/Impl/Force.cs
@@ -12,34 +12,23 @@ namespace SpatialSlur.Dynamics.Impl
     /// <summary>
     /// Base class for a force that acts on the positions and rotations of a dynamic collection of particles.
     /// </summary>
-    public abstract class Force : InfluenceBase<(Vector3d Position, Vector3d Rotation)>, IForce
+    public abstract class Force : Influence<(Vector3d Position, Vector3d Rotation)>, IForce
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        public Force() { }
-
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="handles"></param>
-        public Force(IEnumerable<ParticleHandle> handles) 
-            : base(handles) { }
-
-
         /// <inheritdoc />
         public virtual void Accumulate(
-            ArrayView<Vector3d> forceSum, 
-            ArrayView<Vector3d> torqueSum)
+            ArrayView<Vector3d> forceSums, 
+            ArrayView<Vector3d> torqueSums)
         {
             var handles = Handles;
             var deltas = Deltas;
 
-            for(int i = 0; i < handles.Count; i++)
+            for (int i = 0; i < handles.Count; i++)
             {
-                forceSum[handles[i].PositionIndex] += deltas[i].Position;
-                torqueSum[handles[i].RotationIndex] += deltas[i].Rotation;
+                ref var h = ref handles[i];
+                ref var d = ref deltas[i];
+
+                forceSums[h.PositionIndex] += d.Position;
+                torqueSums[h.RotationIndex] += d.Rotation;
             }
         }
     }
--- SpatialSlur/Dynamics/Impl/RotationForce.cs
+++ SpatialSlur/Dynamics/Impl/RotationForce.cs
@@ -12,32 +12,18 @@ namespace SpatialSlur.Dynamics.Impl
     /// <summary>
     /// Base class for a force that acts on the rotations of a dynamic collection of particles.
     /// </summary>
-    public abstract class RotationForce : InfluenceBase<Vector3d>, IForce
+    public abstract class RotationForce : Influence<Vector3d>, IForce
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        public RotationForce() { }
-
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="handles"></param>
-        public RotationForce(IEnumerable<ParticleHandle> handles)
-            : base(handles) { }
-
-
         /// <inheritdoc />
         public virtual void Accumulate(
-            ArrayView<Vector3d> forceSum, 
-            ArrayView<Vector3d> torqueSum)
+            ArrayView<Vector3d> forceSums, 
+            ArrayView<Vector3d> torqueSums)
         {
             var handles = Handles;
             var deltas = Deltas;
 
             for (int i = 0; i < handles.Count; i++)
-                torqueSum[handles[i].RotationIndex] += deltas[i];
+                torqueSums[handles[i].RotationIndex] += deltas[i];
         }
     }
 }
