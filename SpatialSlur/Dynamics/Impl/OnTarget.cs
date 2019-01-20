4b4aa32196d1b13dcf81be5ac783cd67c4b354de
David Reeves <darthur.reeves@gmail.com>
2019-01-20 02:15:09 +0000

    Add base type for target constraints

---
+++ SpatialSlur/Dynamics/Impl/OnTarget.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Notes
+ */ 
+ 
+using SpatialSlur.Collections;
+
+namespace SpatialSlur.Dynamics.Impl
+{
+    /// <summary>
+    /// Base class for constraints that project groups of particles to target objects
+    /// </summary>
+    public abstract class OnTarget<T> : PositionConstraint
+    {
+        private SlurList<int> _targetIndices = new SlurList<int>();
+        private SlurList<T> _targets = new SlurList<T>();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public SlurList<int> TargetIndices
+        {
+            get => _targetIndices;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public SlurList<T> Targets
+        {
+            get => _targets;
+        }
+    }
+}
